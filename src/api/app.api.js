0d4710cfaa013d76c9826b97a94bd487618e6697
Eli-N-Or 2 <eli-n-or@outlook.com>
2023-11-26 10:33:39 +0200

    switched asset src

--- src/api/app.api.js
+++ src/api/app.api.js
@@ -1,6 +1,6 @@
 const sliderDataUrl = "https://api.npoint.io/2faed63973ec7f79740a";
 const gameDataUrl = "https://api.npoint.io/d51778e00788e90bb674";
-export const assetUrl = "https://redrossent.onrender.com/unagibet";
+export const assetUrl = "https://staticunagibet.netlify.app";
 
 export async function fetchSliderData() {
   const slider_data = await (await fetch(sliderDataUrl)).json();
